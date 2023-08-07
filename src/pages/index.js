415956eb2af134f28138d4fb4b47210f367ceeab
mikihojo <77333363+mikihojo@users.noreply.github.com>
2023-08-07 18:59:41 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        bienvenue à<b>Gatsby!</b>
+        bienvenue à <b>Gatsby!</b>
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}

5b48a76f6daa6efe90e14e59895cb33507ec38d4
mikihojo <77333363+mikihojo@users.noreply.github.com>
2023-08-09 10:34:42 +0900

    Update select-language.js

--- src/pages/select-language.js
+++ src/pages/select-language.js
@@ -8,15 +8,24 @@ import Seo from "../components/seo"
 const SelectPage = () => (
   <Layout>
     <h1>言語の追加</h1>
-    <h2>1.設定画面を開く</h2>
-    <p>設定画面内に「時刻と言語」があります。そこをクリック。</p>
-    <StaticImage
-        src="../images/set.png"
-        loading="eager"
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
+      <h2>1.設定画面を開く</h2>
+        <p>設定画面内に「時刻と言語」があります。そこをクリック。</p>
+        <StaticImage
+          src="../images/set.png"
+          loading="eager"
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+      <h2>2.言語の項目を選択</h2>
+        <p>左側メニューに「言語」の項目があります。そこをクリック。</p>
+        <StaticImage
+          src="../images/time_and_language.png"
+          loading="eager"
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />        
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

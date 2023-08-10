2ed381cd18fb7b27f99d621deddc3354fccbf7d9
mikihojo <77333363+mikihojo@users.noreply.github.com>
2023-08-10 23:41:47 +0900

    Update language-font.js

--- src/pages/language-font.js
+++ src/pages/language-font.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Table from "react-bootstrap/Table"
+import { Table } from "react-bootstrap"
 
 import Layout from "../components/layout"
 

206e3776151365b8daf5c43e30eadd61260339e2
mikihojo <77333363+mikihojo@users.noreply.github.com>
2023-08-10 11:09:04 +0900

    Create language-font.js

---
+++ src/pages/language-font.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+
+const LanguageFont = () => (
+  <Layout>
+    <h1>各言語とフォントの対応</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+
+
+export default LanguageFont

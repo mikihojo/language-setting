bffa3e88d3dc3efdbd7308095bcdfb990ecff050
mikihojo <77333363+mikihojo@users.noreply.github.com>
2023-08-10 11:58:24 +0900

    Update language-font.js

--- src/pages/language-font.js
+++ src/pages/language-font.js
@@ -7,7 +7,16 @@ import Layout from "../components/layout"
 const LanguageFont = () => (
   <Layout>
     <h1>各言語とフォントの対応</h1>
-    <p>Welcome to page 2</p>
+      <p>Welcome to page 2</p>
+      <table>
+        <tr>
+          <td rowspan=”2″>クメール語</td>
+          <td>Leelawadee UI</td>
+        </tr>
+        <tr>
+          <td>Linux Biolinum G</td>
+        </tr>
+      </table>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

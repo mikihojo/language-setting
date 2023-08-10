d16352922042707d9028a8269aabec2a47cfb360
mikihojo <77333363+mikihojo@users.noreply.github.com>
2023-08-10 23:32:19 +0900

    Update language-font.js

--- src/pages/language-font.js
+++ src/pages/language-font.js
@@ -1,8 +1,9 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Table from "react-bootstrap/Table"
 
 import Layout from "../components/layout"
-import Table from "react-bootstrap/Table"
+
 
 
 const LanguageFont = () => (
@@ -20,6 +21,35 @@ const LanguageFont = () => (
       </Table>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
+<Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan={2}>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
 )
 
 

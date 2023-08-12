e3b6b1809432ac7d195fa51eefd096434d81b058
mikihojo <77333363+mikihojo@users.noreply.github.com>
2023-08-12 22:13:26 +0900

    Update select-language.js

--- src/pages/select-language.js
+++ src/pages/select-language.js
@@ -25,7 +25,25 @@ const SelectPage = () => (
           formats={["auto", "webp", "avif"]}
           alt=""
           style={{ marginBottom: `var(--space-3)` }}
-        />        
+        />
+      <h2>3.言語の追加を選択</h2>
+        <p>「言語の追加」クリックして必要な言語を追加します。</p>
+        <StaticImage
+          src="../images/add_language.png"
+          loading="eager"
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+      <h2>4.インストールする言語を選択</h2>
+        <p>インストールする言語を選択して、「次へ」をクリック。</p>
+        <StaticImage
+          src="../images/install_language.png"
+          loading="eager"
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
